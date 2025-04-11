5046336a48bccce249121815292469402d208800
Rabiul Islam <rabiul.robi2023@gmail.com>
2025-04-11 19:41:59 +0600

    A logger middleware added

--- src/redux/store/store.ts
+++ src/redux/store/store.ts
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "../features/counterSlice";
+import logger from "../middleware/logger";
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
   },
+  middleware: (GetDefaultMiddleware) => GetDefaultMiddleware().concat(logger),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
